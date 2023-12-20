c0b290f6b86dfb240ea6fbe35e85ed78ed41f288
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2023-12-20 10:14:49 +0100

    save

--- script.js
+++ script.js
@@ -1,65 +1,83 @@
-let items = document.querySelectorAll('.footer__morpion .morpion__grid ul li button')
+const items = document.getElementsByClassName('ul__button')
 const x = document.getElementsByClassName('open__x-button')
 const o = document.getElementsByClassName('open__o-button')
-let hiden = document.querySelector('.morpion__open')
+const hiden = document.querySelector('.morpion__open')
+const title = document.querySelector('h2')
+const scoreX = document.getElementsByClassName('score__counter-x')
+const scoreO = document.getElementsByClassName('score__counter-o')
 let begin
 
 let checker = (index1, index2, index3, symbol) => {
     return items[index1].innerText.includes(symbol) &&
            items[index2].innerText.includes(symbol) &&
            items[index3].innerText.includes(symbol)
-
 }
 
-x[0].addEventListener('click', function(e){
-    e.preventDefault()
-    begin = 0
-    hiden.style.display='none'
 
-})
+let condition = (symbole) =>{
+   return checker(0,1,2,symbole)||checker(3,4,5,symbole)|| 
+            checker(6,7,8,symbole)|| checker(0,3,6,symbole)|| 
+            checker(1,4,7,symbole)|| checker(2,5,8,symbole)|| 
+            checker(0,4,8,symbole) ||checker(2,4,6,symbole)
+}
 
-o[0].addEventListener('click', function(e){
-    e.preventDefault()
-    begin = 1
-    hiden.style.display='none'
-})
+let handleButtonClick = (button, number) => {
+    button.addEventListener('click', function(e){
+        e.preventDefault()
+        begin = number
+        hiden.style.display='none'
+        title.style.display='none'
+    })
+}
 
+let win = (winner) => {
+    alert(winner)
+    for(let n = 0 ; n < items.length; n++){
+        while (items[n].lastElementChild) {
+            items[n].removeChild(items[n].lastElementChild);
+        }
+    }
+}
 
+let print = (classe, item,letter) => {
+    let p = document.createElement('p')
+            p.classList.add(classe)
+            items[item].appendChild(p)
+            p.innerText=letter
+}
+let printScore = (attribut, value) => {
+    attribut.innerText = value
+}
+
+handleButtonClick(x[0],0)
+handleButtonClick(o[0],1)
+
+let scoreXValue = 0
+let scoreOValue = 0
 for(let i = 0; i < items.length; i++){
-
+   
     items[i].addEventListener('click', function(event){
         event.preventDefault()
-
-
+        
         if( begin === 0 && items[i].innerText == ""){
-
-            let p = document.createElement('p')
-            items[i].appendChild(p)
-            p.innerText='x'
+            print('button__X',i,'x')
             begin += 1
-           if(checker(0,1,2,'x')||checker(3,4,5,'x')|| checker(6,7,8,'x')|| checker(0,3,6,'x')|| checker(1,4,7,'x')|| checker(2,5,8,'x')|| checker(0,4,8,'x') ||checker(2,4,6,'x')){
-                alert('win x')
-                for(let i = 0 ; i < items.length; i++){
-                    p.remove()
-                }
+            if(condition('x')){
+                win('WIN X')
+                scoreXValue ++
+                printScore(scoreX[0],scoreXValue)
             }
         }else if(begin == 1 && items[i].innerText == ""){
-
-            let p = document.createElement('p')
-            items[i].appendChild(p)
-            p.innerText = "o"
+            print('button__O',i,'o')
             begin -= 1
-            if(checker(0,1,2,'o')||checker(3,4,5,'o')|| checker(6,7,8,'o')|| checker(0,3,6,'o')|| checker(1,4,7,'o')|| checker(2,5,8,'o')|| checker(0,4,8,'o') ||checker(2,4,6,'o')){
-                alert('win o')
-                for(let i = 0 ; i < items.length; i++){
-                    p.remove()
-                }
+            if(condition('o')){
+                win('WIN O')
+                scoreOValue ++
+                printScore(scoreO[0],scoreOValue)
             }
         }
+    })}
+    
 
-
-    })
-
-}
 
 
